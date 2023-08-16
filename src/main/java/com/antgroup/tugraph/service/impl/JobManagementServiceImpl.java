1476be01c009e469804ab185ade0a65d9d09e4fd
SonglinLyu <lyusonglin990625@gmail.com>
2023-08-16 14:17:45 +0800

    add handle request

--- src/main/java/com/antgroup/tugraph/service/impl/JobManagementServiceImpl.java
+++ src/main/java/com/antgroup/tugraph/service/impl/JobManagementServiceImpl.java
@@ -6,7 +6,41 @@ import com.baidu.cloud.starlight.springcloud.server.annotation.RpcService;
 public class JobManagementServiceImpl implements JobManagementService {
     @Override
     public TugraphManagement.JobManagementResponse handleRequest(TugraphManagement.JobManagementRequest request) {
-        TugraphManagement.JobManagementResponse resp = TugraphManagement.JobManagementResponse.newBuilder().build();
+        TugraphManagement.JobManagementResponse resp;
+
+        switch(request.getReqCase()){
+            case CREAT_JOB_REQUEST :
+                resp = handleCreatJobRequest(request);
+            break;
+            case READ_JOB_REQUEST :
+                resp = handleReadJobRequest(request);
+            break;
+            case UPDATE_JOB_REQUEST :
+                resp = handleUpdateJobRequest(request);
+            break;
+            case DELETE_JOB_REQUEST :
+                resp = handleDeleteJobRequest(request);
+            break;
+            default:
+                resp = TugraphManagement.JobManagementResponse.newBuilder().setErrorCode(TugraphManagement.JobManagementResponse.ErrorCode.FAILED).build();
+        }
+
         return resp;
+    }
+
+    public TugraphManagement.JobManagementResponse handleCreatJobRequest(TugraphManagement.JobManagementRequest request) {
+        return TugraphManagement.JobManagementResponse.newBuilder().setErrorCode(TugraphManagement.JobManagementResponse.ErrorCode.FAILED).build();
+    }
+
+    public TugraphManagement.JobManagementResponse handleReadJobRequest(TugraphManagement.JobManagementRequest request) {
+        return TugraphManagement.JobManagementResponse.newBuilder().setErrorCode(TugraphManagement.JobManagementResponse.ErrorCode.FAILED).build();
+    }
+
+    public TugraphManagement.JobManagementResponse handleUpdateJobRequest(TugraphManagement.JobManagementRequest request) {
+        return TugraphManagement.JobManagementResponse.newBuilder().setErrorCode(TugraphManagement.JobManagementResponse.ErrorCode.FAILED).build();
+    }
+
+    public TugraphManagement.JobManagementResponse handleDeleteJobRequest(TugraphManagement.JobManagementRequest request) {
+        return TugraphManagement.JobManagementResponse.newBuilder().setErrorCode(TugraphManagement.JobManagementResponse.ErrorCode.FAILED).build();
     }
 }
