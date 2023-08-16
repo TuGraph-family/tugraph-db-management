c8c33d9161a288b03f06e58de23bf6ec6aa28824
SonglinLyu <lyusonglin990625@gmail.com>
2023-08-16 14:17:21 +0800

    new springboot project

---
+++ src/main/java/com/antgroup/tugraph/TugraphManagementApplication.java
@@ -0,0 +1,13 @@
+package com.antgroup.tugraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugraphManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugraphManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antgroup/tugraph/TugraphManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antgroup.tugraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugraphManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
