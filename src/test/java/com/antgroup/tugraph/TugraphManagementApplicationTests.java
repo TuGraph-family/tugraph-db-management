2b1b74c9a154a89d3a6956418d8e86f470554c73
SonglinLyu <lyus@usc.edu>
2023-08-30 21:33:26 +0000

    test unit test

--- src/test/java/com/antgroup/tugraph/TugraphManagementApplicationTests.java
+++ src/test/java/com/antgroup/tugraph/TugraphManagementApplicationTests.java
@@ -1,13 +1,18 @@
 package com.antgroup.tugraph;
 
 import org.junit.jupiter.api.Test;
+import org.junit.Assert;
 import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
+@Slf4j
 class TugraphManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("开始单元测试");
+		Assert.assertEquals(0, 1);
 	}
 
 }
