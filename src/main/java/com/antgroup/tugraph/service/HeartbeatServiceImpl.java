33edb7dcc95706efc4d1e0f66ca9ea51a33376a0
SonglinLyu <lyus@usc.edu>
2023-10-05 00:21:33 +0000

    add heartbeat detect

---
+++ src/main/java/com/antgroup/tugraph/service/HeartbeatServiceImpl.java
@@ -0,0 +1,23 @@
+package com.antgroup.tugraph;
+
+import com.baidu.cloud.starlight.springcloud.server.annotation.RpcService;
+import lombok.extern.slf4j.Slf4j;
+
+@RpcService
+@Slf4j
+public class HeartbeatServiceImpl implements HeartbeatService {
+
+    @Override
+    public TuGraphDBManagement.HeartbeatResponse detectHeartbeat(TuGraphDBManagement.HeartbeatRequest request) {
+        log.info("get heartbeat request");
+        String reqMsg = request.getRequestMsg();
+        log.info(reqMsg);
+        String respMsg = "This is a heartbeat response message.";
+        TuGraphDBManagement.HeartbeatResponse.Builder respBuilder =
+            TuGraphDBManagement.HeartbeatResponse
+                .newBuilder();
+        respBuilder.setResponseMsg(respMsg);
+        return respBuilder.build();
+    }
+
+}
