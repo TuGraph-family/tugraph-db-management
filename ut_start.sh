aed036f3c22b29503df8211648048d42498c40a5
SonglinLyu <lyus@usc.edu>
2023-10-18 20:33:11 +0000

    fix ut, fix proto

--- ut_start.sh
+++ ut_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup mvn spring-boot:run -Dspring-boot.run.arguments="--spring.profiles.active=dev" > log.txt 2>&1 & echo $! > pidfile.txt
+nohup mvn spring-boot:run -Dspring-boot.run.arguments="--spring.profiles.active=ut" > log.txt 2>&1 & echo $! > pidfile.txt
 touch tugraph_db_management_ut.db
 
