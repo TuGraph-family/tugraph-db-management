184bc84920a592158bd0691fecfc1322bab1fef4
SonglinLyu <lyus@usc.edu>
2023-10-19 06:32:05 +0000

    fix ut

--- ut_stop.sh
+++ ut_stop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+mvn spring-boot:stop
 kill -9 `cat pidfile.txt`
+kill -9 `ps -ef | grep spring.profiles.active=ut | grep -v "grep" | awk '{print $2}'`
 rm -f pidfile.txt
 rm -f log.txt
 rm -f tugraph_db_management_ut.db
