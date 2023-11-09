c8b088b5ea5f710428987c8a1a32faf3b9a8fac9
qishipengqsp <shepherdkingqsp@gmail.com>
2023-11-09 15:02:28 +0800

    add local build

---
+++ local_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# env config
+export MAVEN_OPTS=-Xss10m
+mkdir -p ~/.m2
+echo "<settings>
+  <mirrors>
+    <mirror>
+      <id>alimaven</id>
+      <mirrorOf>central</mirrorOf>
+      <url>http://maven.aliyun.com/nexus/content/groups/public/</url>
+    </mirror>
+  </mirrors>
+</settings>" > ~/.m2/settings.xml
+# build all
+mvn --batch-mode --update-snapshots --no-transfer-progress clean -f pom.xml package -DskipTests -Dmaven.javadoc.skip -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN
--- ut_stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kill -9 `cat pidfile.txt`
-kill -9 `ps -ef | grep spring.profiles.active=ut | grep -v "grep" | awk '{print $2}'`
-rm -f pidfile.txt
-rm -f log.txt
-rm -f tugraph_db_management_ut.db
